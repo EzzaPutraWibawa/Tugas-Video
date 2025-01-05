2cd5c48af73611b29cfe3b9ba1630f66b0d029a4
Zaa <ezzapwz@gmail.com>
2025-01-05 23:15:41 +0800

    update message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps!"
+    return "Hello, DevOps!, saya sedang membuat tugas video MJS"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
